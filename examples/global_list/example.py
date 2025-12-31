00cb8d8de37533a483b5baccf4ce42ac35aec72a
Sander Bos <sander@streamhpc.com>
2025-12-31 22:05:54 +0100

    feat: add examples/global_list

---
+++ examples/global_list/example.py
@@ -0,0 +1,14 @@
+import grug
+from grug.packages import grug_stdlib
+
+state = grug.init(
+    packages=[
+        grug_stdlib.get(),
+    ]
+)
+
+file = state.compile_grug_file("animals/labrador-Dog.grug")
+dog1 = file.create_entity()
+
+dog1.on_run()
+dog1.on_run()
