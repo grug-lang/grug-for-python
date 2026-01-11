26a47d4594f02c9a1d25eff2e5968cdf9f7b0ab5
Sander Bos <sander@streamhpc.com>
2026-01-11 22:09:59 +0100

    tests: let tests.py print more

--- tests.py
+++ tests.py
@@ -5,12 +5,10 @@ from pathlib import Path
 def run_examples():
     for example in Path("examples").glob("*/example.py"):
         example_dir = example.parent
+        print(f"\nRunning {example_dir}...")
         try:
             subprocess.run(
-                ["python", example.name],
-                cwd=example_dir,
-                timeout=3,
-                check=False,  # Ignore exit code
+                ["python", example.name], cwd=example_dir, timeout=3, check=True
             )
         except subprocess.TimeoutExpired:
             pass
@@ -19,6 +17,7 @@ def run_examples():
 def run_package_tests():
     for test_file in Path("src/grug/packages").glob("**/tests/tests.py"):
         test_dir = test_file.parent
+        print(f"\nRunning {test_dir}...")
         subprocess.run(
             ["python", test_file.name],
             cwd=test_dir,
@@ -27,5 +26,6 @@ def run_package_tests():
 
 
 if __name__ == "__main__":
+    print("Running all example programs and package tests...")
     run_examples()
     run_package_tests()
