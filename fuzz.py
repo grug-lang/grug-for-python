4128dc3a0b9b477d887cfa390da1a785abbf6306
Sander Bos <sander@streamhpc.com>
2026-01-25 23:43:36 +0100

    ci: wrap afl-fuzz in timed out shell

--- fuzz.py
+++ fuzz.py
@@ -1,12 +1,8 @@
 import os
 import sys
 
-BUF_SIZE = 4096
+data = sys.stdin.buffer.read()
 
-while True:
-    data = sys.stdin.buffer.read(BUF_SIZE)
-    if not data:
-        break
-    if data == b"ab":
-        print("python: crash!", file=sys.stderr)
-        os.abort()
+if data == b"ab":
+    print("python: crash!", file=sys.stderr)
+    os.abort()
