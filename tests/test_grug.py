117fa362368c59cde5d233f0539d6569dc575ab5
NikhilNathanael <nikhilnathanael2020@gmail.com>
2026-06-04 01:02:25 +0530

    trying to fix leak sanitizer

--- tests/test_grug.py
+++ tests/test_grug.py
@@ -120,7 +120,7 @@ def test_grug(
     id_map: dict[int, GrugFile] = {}
 
     current_entity: Optional[Entity] = None
-    error_buffers: List[bytes] = []
+    _last_error: Optional[bytes] = None
 
     @ctypes.CFUNCTYPE(
         ctypes.c_void_p,
@@ -143,11 +143,10 @@ def test_grug(
             id_map[file_id] = grug_file
             return file_id
         except Exception as e:
-            buf = str(e).encode()
             # This ensures the buffer returned from this function isnt cleaned
             # up before the c code has a chance to use it
-            error_buffers.append(buf)
-            out_err[0] = buf
+            _last_error = str(e).encode();
+            out_err[0] = _last_error
             return -1
 
     @ctypes.CFUNCTYPE(None, ctypes.c_void_p, ctypes.c_void_p)
