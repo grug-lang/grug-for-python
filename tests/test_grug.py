df5026d65e7af52e8c9153c81eccf86c8e39ba1c
NikhilNathanael <nikhilnathanael2020@gmail.com>
2026-06-04 00:45:15 +0530

    returned error string is stored in array to ensure it is not garbage collected

--- tests/test_grug.py
+++ tests/test_grug.py
@@ -120,6 +120,7 @@ def test_grug(
     id_map: dict[int, GrugFile] = {}
 
     current_entity: Optional[Entity] = None
+    error_buffers: [bytes] = []
 
     @ctypes.CFUNCTYPE(
         ctypes.c_void_p,
@@ -142,7 +143,11 @@ def test_grug(
             id_map[file_id] = grug_file
             return file_id
         except Exception as e:
-            out_err[0] = str(e).encode()
+            buf = str(e).encode()
+            # This ensures the buffer returned from this function isnt cleaned
+            # up before the c code has a chance to use it
+            error_buffers.add(buf)
+            out_err[0] = buf
             return -1
 
     @ctypes.CFUNCTYPE(None, ctypes.c_void_p, ctypes.c_void_p)
