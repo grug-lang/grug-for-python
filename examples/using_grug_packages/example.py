4f6de0484fd058cf564cfd24ddf5f7925924da11
NikhilNathanael <nikhilnathanael2020@gmail.com>
2026-08-15 22:18:00 +0530

    fixed unnecessary no cover pragmas

--- examples/using_grug_packages/example.py
+++ examples/using_grug_packages/example.py
@@ -2,11 +2,31 @@ import time
 
 import grug
 from grug.packages import grug_numpy, grug_stdlib
+from grug import GrugPackage
+
+class Printer:
+    @staticmethod
+    def print_string(state: GrugState, self, string: str):
+        print(string)
+
+
+def printer(state: GrugState) -> Printer:
+    return Printer()
+
+my_package = GrugPackage(
+    prefix="",
+    host_fns=[printer],
+    generic_fns=[],
+    methods=[("Printer", Printer.print_string)],
+    generic_methods=[],
+)
+
 
 state = grug.init(
     packages=[
         grug_stdlib.get(),
         grug_numpy.get(),
+        my_package,
     ]
 )
 
